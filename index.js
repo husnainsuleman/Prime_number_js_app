9a6f47ebe8aaa59f416c7c04d829aa4777648625
Muhammad Husnain <51457254+husnainsuleman@users.noreply.github.com>
2020-06-26 09:29:26 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Prime Number Checker//
+
+// var bf= true;
+// var pum =parseInt(prompt(""));
+// for (var b2=2; b2<10; b2++){
+//     var result= pum % b2;
+//     if (result==0){
+//         console.log("Number is nOt Prime " +pum);
+//         bf=false;   
+//         break;
+//    }
+// } 
+// if (bf){
+//    console.log("Number is Prime"+ num);
+// }
+
+//Prime Number//
